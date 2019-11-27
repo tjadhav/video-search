ee4d83cdbebf52ae4e636eff4dbc578101cdaa92
tjadhav <tusharjadhav219@gmail.com>
2019-11-27 20:30:20 +0530

    Updates

---
+++ precache-manifest.e79f6db09a1ab41dbfd6b8a54ecb8f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6763ef0ded8c04732aa10969c2bcd84",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "33fab642d70a3fd0ac3d",
+    "url": "/video-search/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "459104914f279ac905e3",
+    "url": "/video-search/static/js/2.185c2061.chunk.js"
+  },
+  {
+    "revision": "33fab642d70a3fd0ac3d",
+    "url": "/video-search/static/js/main.432e8a53.chunk.js"
+  },
+  {
+    "revision": "6380d8bba41c20c5534e",
+    "url": "/video-search/static/js/runtime-main.4ee03923.js"
+  }
+]);
